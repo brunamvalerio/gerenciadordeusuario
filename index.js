c73c349d2024491352eda18b16c66eab897c9897
brunamvalerio <136118067+brunamvalerio@users.noreply.github.com>
2025-01-23 11:54:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-let userController = new UserController("form-user-create", "form-user-update", "table-users")
+let userController = new UserController("form-user-create", "form-user-update", "table-users") //// Aqui estamos passando dois parâmetros para a classe 'UserController':
+// - "form-user-create": O ID do formulário onde os dados do usuário serão inseridos
+// - "table-users": O ID da tabela dos usuários
